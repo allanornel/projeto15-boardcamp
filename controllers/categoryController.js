4891d2d6e3180c40440581165c8d0e3a0a09b2d0
Allan Ornel <allanornel@gmail.com>
2022-05-25 20:33:54 -0300

    feat: db connection with Postgres done and get /categories done

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,13 @@
 import db from "./../db.js";
 
-export async function getCategories(req, res) {}
+export async function getCategories(req, res) {
+  try {
+    const categories = await db.query("SELECT * FROM categories");
+    res.status(200).send(categories.rows);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Ocorreu um erro ao obter as categorias");
+  }
+}
 
 export async function postCategories(req, res) {}
--- db.js
+++ db.js
@@ -1,7 +1,12 @@
 import dotenv from "dotenv";
+import pg from "pg";
 
 dotenv.config();
 
-const connection = new Pool({
+const { Pool } = pg;
+
+const db = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
+
+export default db;
